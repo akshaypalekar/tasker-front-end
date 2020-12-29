b994c26c6fc6ee4fe57e2117b3ddfb40aeee0b2e
Akshay Palekar <apalekar@jahnelgroup.com>
2020-12-29 07:22:03 -0500

    Building project and pushing to PROD

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,10 @@
 export const environment = {
   production: true,
   name: "PROD",
-  endpoint: "https://vaf8rc22r6.execute-api.us-east-1.amazonaws.com/Prod/"
+  endpoint: "https://vaf8rc22r6.execute-api.us-east-1.amazonaws.com/Prod/",
+  api_endpoint: 'https://tddowj8otk.execute-api.us-east-1.amazonaws.com/dev/items',
+  sso_endpoint: 'https://taskerdomain.auth.us-east-1.amazoncognito.com',
+  clientId: '19ul0dh7n2d7mdfdf5m6tnds9o',
+  region: 'us-east-1',
+  userpoolId: 'us-east-1_FBce5QEQc'
 };
