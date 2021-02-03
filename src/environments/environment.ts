83009f3256958fa75cda6ab6e438ce1bd809cafe
Akshay Palekar <akshaypalekar91@gmail.com>
2021-02-03 08:02:55 -0500

    Update environment.ts
    Removing client keys

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   name: "DEV",
-  endpoint: "https://vaf8rc22r6.execute-api.us-east-1.amazonaws.com/Prod/",
-  api_endpoint: 'https://tddowj8otk.execute-api.us-east-1.amazonaws.com/dev/items',
-  sso_endpoint: 'https://taskerdomain.auth.us-east-1.amazoncognito.com',
-  clientId: '19ul0dh7n2d7mdfdf5m6tnds9o',
-  region: 'us-east-1',
-  userpoolId: 'us-east-1_FBce5QEQc'
+  endpoint: "",
+  api_endpoint: '',
+  sso_endpoint: '',
+  clientId: '',
+  region: '',
+  userpoolId: ''
 };
 
 /*
